59b3869b479cda9ae17416182c929f4d0774d372
João Vítor Retucci <jvitor.200003@gmail.com>
2023-04-24 23:10:59 -0300

    fix: fix hero image path

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,7 +6,7 @@ export const Hero: FC = (): JSX.Element => {
     <div id="hero">
       <h1 className="text-3xl font-bold uppercase text-center mb-4">Estamos procurando a Dory!</h1>
       <div className="relative w-full aspect-square border-amber-600 border-8 rounded-md">
-        <Image src="/assets/images/doritos.jpg" alt="Fotinha da Dory" className="object-cover" fill/>
+        <Image src="/assets/images/dory/doritos.jpg" alt="Fotinha da Dory" className="object-cover" fill/>
       </div>
     </div>
   );
